acf67b9759f4c21913e3433b99ceeef320a66a4b
Himanshu Negi <negihimanshu015@gmail.com>
2024-09-11 17:52:49 +0530

    added texture

--- src/Components/earth.jsx
+++ src/Components/earth.jsx
@@ -1,13 +1,27 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
+import { TextureLoader } from "three";
 
 function Earth() {
-    const scene = useRef();
-  return (    
-    <Canvas ref={scene} >
+  const scene = useRef();
+  const [chessTexture, normal] = useLoader(TextureLoader, [
+    "/earth_texture.jpg",
+    "/clouds.jpg",
+  ]);
+  return (
+    <Canvas ref={scene}>
       <ambientLight intensity={2} />
-      <mesh scale={2.5}>
-        <sphereGeometry args={[1, 64, 64]}/>
+      <mesh scale={3}>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial map={chessTexture} />
+      </mesh>
+      <mesh scale={3}>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial
+          map={normal}
+          transparent={true} // Enable transparency
+          opacity={0.5} // Adjust opacity for better visual
+        />
       </mesh>
     </Canvas>
   );
