2b4855913f4438820a316f873a3b5611c647ea66
Himanshu Negi <negihimanshu015@gmail.com>
2024-09-12 18:58:01 +0530

    added title & login

--- src/Components/earth.jsx
+++ src/Components/earth.jsx
@@ -6,8 +6,8 @@ import { OrbitControls } from "@react-three/drei";
 function Earth() {  
   const EarthRef = useRef();
   const CloudRef = useRef();  
-  const [chessTexture, normal] = useLoader(TextureLoader, [
-    "/day.jpg",
+  const [earthTexture, normal] = useLoader(TextureLoader, [
+    "/blackw.jpg",
     "/clouds.jpg",
   ]);
   
@@ -28,7 +28,7 @@ function Earth() {
       <ambientLight intensity={2} />
       <mesh scale={3} ref={EarthRef}>
         <sphereGeometry args={[1, 64, 64]} />
-        <meshStandardMaterial map={chessTexture} />        
+        <meshStandardMaterial map={earthTexture} />        
       </mesh>
       <mesh scale={3} ref={CloudRef}>
         <sphereGeometry args={[1.008, 64, 64]} />
@@ -37,13 +37,14 @@ function Earth() {
           transparent={true}
           opacity={0.5}
         />
-      </mesh>      
+      </mesh>                     
     </>
   );
 }
 
 function EarthScene() {
   return (
+    <>    
     <Canvas>
       <ambientLight intensity={2} />
       <Earth />
@@ -55,7 +56,9 @@ function EarthScene() {
         panSpeed={0.5} 
         rotateSpeed={0.1} 
       />
-    </Canvas>
+    </Canvas>    
+    </>
+    
   );
 }
 
--- src/Front.jsx
+++ src/Front.jsx
@@ -4,7 +4,11 @@ import Earth from "./Components/earth";
 function App() {
   return (
     <main className="main">
-      <Earth />      
+      <h1 className="title is-size-1">ClimeVizion</h1>      
+      <Earth />
+      <div className="center">
+      <button className="button is-white is-outlined is-large is-rounded ">Login &#8594;</button>
+    </div>                
     </main>
   );
 }
