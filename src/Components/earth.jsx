66f707cf85a8fe409cf2bd375b99d208b5a4f895
Himanshu Negi <negihimanshu015@gmail.com>
2024-09-13 19:27:37 +0530

    Front Page completed.

--- src/Components/earth.jsx
+++ src/Components/earth.jsx
@@ -1,16 +1,16 @@
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
 import { TextureLoader } from "three";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 
-function Earth() {  
+function Earth() {
   const EarthRef = useRef();
-  const CloudRef = useRef();  
+  const CloudRef = useRef();
   const [earthTexture, normal] = useLoader(TextureLoader, [
-    "/blackw.jpg",
+    "/day.jpg",
     "/clouds.jpg",
   ]);
-  
+
   useFrame(() => {
     if (EarthRef.current) {
       EarthRef.current.rotation.y += 0.001; // Rotate the Earth mesh around the y-axis
@@ -22,45 +22,41 @@ function Earth() {
       CloudRef.current.rotation.y += 0.001; // Rotate the Earth mesh around the y-axis
     }
   });
-  
+
   return (
     <>
-      <ambientLight intensity={2} />
+      <Stars radius={600} depth={100} count={20000} factor={1} fade={true} />
+      <ambientLight intensity={1} />
       <mesh scale={3} ref={EarthRef}>
         <sphereGeometry args={[1, 64, 64]} />
-        <meshStandardMaterial map={earthTexture} />        
+        <meshStandardMaterial map={earthTexture} />
       </mesh>
       <mesh scale={3} ref={CloudRef}>
         <sphereGeometry args={[1.008, 64, 64]} />
-        <meshStandardMaterial
-          map={normal}
-          transparent={true}
-          opacity={0.5}
-        />
-      </mesh>                     
+        <meshStandardMaterial map={normal} transparent={true} opacity={0.5} />
+      </mesh>
     </>
   );
 }
 
 function EarthScene() {
   return (
-    <>    
-    <Canvas>
-      <ambientLight intensity={2} />
-      <Earth />
-      <OrbitControls 
-        enableZoom={true} 
-        enablePan={true} 
-        enableRotate={true} 
-        zoomSpeed={0.6} 
-        panSpeed={0.5} 
-        rotateSpeed={0.1} 
-      />
-    </Canvas>    
+    <>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={7} position={[1, 0, 0.25]} />
+        <Earth />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.1}
+        />
+      </Canvas>
     </>
-    
   );
 }
-
 
 export default EarthScene;
