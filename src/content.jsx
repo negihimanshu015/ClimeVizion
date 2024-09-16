4b70460e95c091c7dbe571657816dedc40abe753
Himanshu Negi <negihimanshu015@gmail.com>
2024-09-16 19:50:22 +0530

    Added content page.

--- src/content.jsx
+++ src/content.jsx
@@ -1,9 +1,23 @@
-import "./index.css"
+import "./index.css";
+import "./styles/content.css";
 
 function Content() {
-return(
-    <h1>hello</h1>
-);
+  return (
+    <div className="content">
+        <div className="columns">
+            <div className="column is-one-quarter center">
+                <div className="card">
+                    <div className="box p-6">
+                    <p className=" p-3 m-3">Location : </p>
+                    <input class="input" type="text" placeholder="Text input" />
+                    <p className="p-3 m-3">Time-Period : </p>                    
+                    <button className="button m-3">Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
